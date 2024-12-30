9482ae8e1c43041687d756939b053a9967afd527
Tijo <hamsen3880@outlook.com>
2024-12-31 02:51:44 +0530

    Build Fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import TopBackground from './components/Hero/TopBackground'
 import Navbar from './components/Navbar'
-import Projects from './components/Projects/Projects'
 import Sections from './components/Sections'
 
 export default function Home() {
