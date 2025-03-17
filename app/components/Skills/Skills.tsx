2e8435c342137d4979e15b456a34df66e7841d88
Tijo <hamsen3880@outlook.com>
2025-03-18 03:18:12 +0530

    fixed a part

--- app/components/Skills/Skills.tsx
+++ app/components/Skills/Skills.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import skills from '@/data/skills.json'
 import SectionContainer from '../Section/SectionContainer'
 import SectionHeader from '../Section/SectionHeader'
@@ -13,42 +12,42 @@ const Skills = () => {
         <SectionHeader plainText='These are my explored ' highlightText='Technologies' />
         <div className='card w-full px-[33px] py-[33px] flex flex-col flex-wrap md:flex-row justify-center items-center gap-[19px] md:gap-[33px]'>
           <section className='hidden md:flex flex-wrap justify-center items-center gap-[19px] md:gap-[33px]'>
-          <div className="w-full md:h-[35px] text-center font-bold text-2xl font-mono">Programming Languages</div>
-          {
-            skills.slice(0,11).map((skill, id) => (
-              <Skill key={id} name={skill.name} icon={skill.icon} />
-            ))
-          }
-          <div className="w-full md:h-[35px] text-center font-bold text-2xl font-mono">Development (Web and Application) </div>
-          {
-            skills.slice(11,32).map((skill, id) => (
-              <Skill key={id} name={skill.name} icon={skill.icon} />
-            ))
-          }
-          <div className="w-full md:h-[35px] text-center font-bold text-2xl font-mono">Cloud Computing</div>
-          {
-            skills.slice(32,37).map((skill, id) => (
-              <Skill key={id} name={skill.name} icon={skill.icon} />
-            ))
-          }
-          <div className="w-full md:h-[35px] text-center font-bold text-2xl font-mono">Dev Ops</div>
-          {
-            skills.slice(37,45).map((skill, id) => (
-              <Skill key={id} name={skill.name} icon={skill.icon} />
-            ))
-          }
-          <div className="w-full md:h-[35px] text-center font-bold text-2xl font-mono">UI / UX</div>
-          {
-            skills.slice(45,47).map((skill, id) => (
-              <Skill key={id} name={skill.name} icon={skill.icon} />
-            ))
-          }
-          <div className="w-full md:h-[35px] text-center font-bold text-2xl font-mono">Web 3.0</div>
-          {
-            skills.slice(47,48).map((skill, id) => (
-              <Skill key={id} name={skill.name} icon={skill.icon} />
-            ))
-          }
+            <div className="w-full md:h-[35px] text-center font-bold text-2xl font-mono">Programming Languages</div>
+            {
+              skills.slice(0,11).map((skill, id) => (
+                <Skill key={id} name={skill.name} icon={skill.icon} />
+              ))
+            }
+            <div className="w-full md:h-[35px] text-center font-bold text-2xl font-mono">Development (Web and Application) </div>
+            {
+              skills.slice(11,32).map((skill, id) => (
+                <Skill key={id} name={skill.name} icon={skill.icon} />
+              ))
+            }
+            <div className="w-full md:h-[35px] text-center font-bold text-2xl font-mono">Cloud Computing</div>
+            {
+              skills.slice(32,37).map((skill, id) => (
+                <Skill key={id} name={skill.name} icon={skill.icon} />
+              ))
+            }
+            <div className="w-full md:h-[35px] text-center font-bold text-2xl font-mono">Dev Ops</div>
+            {
+              skills.slice(37,45).map((skill, id) => (
+                <Skill key={id} name={skill.name} icon={skill.icon} />
+              ))
+            }
+            <div className="w-full md:h-[35px] text-center font-bold text-2xl font-mono">UI / UX</div>
+            {
+              skills.slice(45,47).map((skill, id) => (
+                <Skill key={id} name={skill.name} icon={skill.icon} />
+              ))
+            }
+            <div className="w-full md:h-[35px] text-center font-bold text-2xl font-mono">Web 3.0</div>
+            {
+              skills.slice(47,48).map((skill, id) => (
+                <Skill key={id} name={skill.name} icon={skill.icon} />
+              ))
+            }
           </section>
         </div>
       </div>
